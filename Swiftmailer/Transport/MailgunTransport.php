29e4c74da7413c8a6100ad76602c15528a7ae925
Fred <fred.luhamaa@gmail.com>
2020-09-02 13:51:30 +0300

    Fix Swift_Mime_SimpleMessage

--- Swiftmailer/Transport/MailgunTransport.php
+++ Swiftmailer/Transport/MailgunTransport.php
@@ -1,7 +1,17 @@
 <?php
 
-namespace MauticPlugin\AMMailgunBundle\Swiftmailer\Transport;
+/*
+ * @copyright   2019 Arrowfunxtion. All rights reserved
+ * @author      Muhammad Azamuddin<mas.azamuddin@gmail.com>
+ *
+ * @link        http://arrowfunxtion.com/international
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
+namespace MauticPlugin\AFMailgunBundle\Swiftmailer\Transport;
+
+use Mautic\CoreBundle\Helper\ArrayHelper;
 use Mautic\EmailBundle\Model\TransportCallback;
 use Mautic\EmailBundle\Swiftmailer\Transport\CallbackTransportInterface;
 use Mautic\LeadBundle\Entity\DoNotContact;
@@ -30,10 +40,12 @@ class MailgunTransport extends \Swift_SmtpTransport implements CallbackTransport
     /**
      * {@inheritdoc}
      */
-    public function __construct(TransportCallback $transportCallback, $sandboxMode = false, $sandboxMail = '')
+    public function __construct(TransportCallback $transportCallback, $host = 'smtp.mailgun.org',  $username = null, $password = null, $sandboxMode = false, $sandboxMail = '', $port = 587)
     {
-        parent::__construct('smtp.mailgun.org', 587, 'tls');
+        parent::__construct($host, $port, 'tls');
         $this->setAuthMode('login');
+        $this->setUsername($username);
+        $this->setPassword($password);
 
         $this->setSandboxMode($sandboxMode);
         $this->setSandboxMail($sandboxMail);
@@ -98,15 +110,21 @@ class MailgunTransport extends \Swift_SmtpTransport implements CallbackTransport
         }
 
         foreach ($events as $event) {
-            if ( ! in_array($event['event'], ['bounce', 'rejected', 'complained', 'unsubscribed', 'permanent_fail', 'failed'])) {
+            if (!in_array($event['event'], ['bounce', 'rejected', 'complained', 'unsubscribed', 'permanent_fail', 'failed'])) {
                 continue;
             }
-
+            $reason = $event['event'];
             if ($event['event'] === 'bounce' || $event['event'] === 'rejected' || $event['event'] === 'permanent_fail' || $event['event'] === 'failed') {
-                $reason = $event['recipient'] . ': ' . $event['reason'];
+                if (!empty($event['delivery-status']['message'])) {
+                    $reason = $event['delivery-status']['message'];
+                }elseif (!empty($event['delivery-status']['description'])) {
+                    $reason = $event['delivery-status']['description'];
+                }
                 $type = DoNotContact::BOUNCED;
             } elseif ($event['event'] === 'complained') {
-                $reason = 'User reported email as spam, source: ' . $event['source'];
+                if (isset($event['delivery-status']['message'])) {
+                    $reason = $event['delivery-status']['message'];
+                }
                 $type = DoNotContact::UNSUBSCRIBED;
             } elseif ($event['event'] === 'unsubscribed') {
                 $reason = 'User unsubscribed';
