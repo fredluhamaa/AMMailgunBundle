8954b4fc21841b3bd703235acbbab1a9a62207f5
Fred <fred.luhamaa@gmail.com>
2020-09-02 14:12:50 +0300

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -1,43 +1,24 @@
 <?php
-
-/*
- * @copyright   2019 Arrowfunxtion. All rights reserved
- * @author      Muhammad Azamuddin<mas.azamuddin@gmail.com>
- *
- * @link        http://arrowfunxtion.com/international
- *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
-
-
 return [
-    'services' => [
+    'services'   => [
         'other' => [
             'mautic.transport.mailgun' => [
-                'class' => \MauticPlugin\AMMailgunBundle\Swiftmailer\Transport\MailgunTransport::class,
-                'arguments' => [
+                'class'        => 'MauticPlugin\AMMailgunBundle\Swiftmailer\Transport\MailgunTransport',
+                'serviceAlias' => 'swiftmailer.mailer.transport.%s',
+                'arguments'    => [
                     'mautic.email.model.transport_callback',
-                    '%mautic.mailer_host%',
-                    '%mautic.mailer_user%',
-                    '%mautic.mailer_port%',
-                    '%mautic.mailer_password%',
                     '%mautic.mailer_mailgun_sandbox%',
                     '%mautic.mailer_mailgun_sandbox_default_mail%',
                 ],
-                'tagArguments' => [
-                    \Mautic\EmailBundle\Model\TransportType::TRANSPORT_ALIAS => 'mautic.email.config.mailer_transport.mailgun',
-                    \Mautic\EmailBundle\Model\TransportType::FIELD_HOST   => true,
-                    \Mautic\EmailBundle\Model\TransportType::FIELD_USER      => true,
-                    \Mautic\EmailBundle\Model\TransportType::FIELD_PASSWORD      => true,
-                    \Mautic\EmailBundle\Model\TransportType::FIELD_PORT      => true,
+                'methodCalls'  => [
+                    'setUsername' => ['%mautic.mailer_user%'],
+                    'setPassword' => ['%mautic.mailer_password%'],
                 ],
-                'tag'          => 'mautic.email_transport',
-                'serviceAlias' => 'swiftmailer.mailer.transport.%s',
             ],
         ],
     ],
     'parameters' => [
-        'mailer_mailgun_sandbox' => false,
+        'mailer_mailgun_sandbox'              => false,
         'mailer_mailgun_sandbox_default_mail' => null,
     ],
 ];
